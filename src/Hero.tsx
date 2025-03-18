5f57fdcb2feb818d69bcd70fbd5d8c31761088f3
Richards Robin R <richardsrobin.r15@gmail.com>
2025-03-18 14:32:58 +0000

    Add dist submodule

---
+++ src/Hero.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import HeroTyping from "./HeroTying.tsx";
+
+const Hero = () => {
+    return(
+        <div className="h-screen w-full flex flex-col items-center justify-center px-8 ">
+            
+            <motion.h1
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.4,
+                scale: { type: "spring", bounce: 0.5 },
+            }}
+            className="text-5xl text-white text-center md:text-8xl font-bold"
+            >
+                Hi, I'm <span className="text-emerald-500">Richards Robin R</span>
+
+            </motion.h1>
+        
+            <HeroTyping/>
+            
+        </div>
+    );
+}
+
+export default Hero;
+
+
+
